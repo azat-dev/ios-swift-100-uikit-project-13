eaaf5f52abca42a9f2446be5fcee4e3b892bee89
Azat <kaumov.a.r@gmail.com>
2021-06-09 22:59:09 +0300

    Initial commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Azat Kaiumov on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
