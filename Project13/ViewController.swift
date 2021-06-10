bdf9aba3b79bfa3c0f365a50e01c5954f567a5c3
Azat <kaumov.a.r@gmail.com>
2021-06-10 15:01:26 +0300

    Add effects

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -7,22 +7,136 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    private var context: CIContext!
+    private var currentFilter: CIFilter!
+    private var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
     
     @IBAction func intensityChanged(_ sender: UISlider) {
+        applyProcessing()
     }
     
     @IBAction func save(_ sender: UIButton) {
     }
     
+    func setFilter(_ action: UIAlertAction) {
+        guard currentImage != nil else {
+            return
+        }
+        
+        guard let title = action.title else {
+            return
+        }
+        
+        currentFilter = CIFilter(name: title)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+    }
+    
     @IBAction func changeFilter(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Choose effect",
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        alert.addAction(.init(title: "Cancel", style: .cancel))
+        
+        let effects = [
+            "CIBumpDistortion",
+            "CIGaussianBlur",
+            "CIPixellate",
+            "CISepiaTone",
+            "CITwirlDistortion",
+            "CIUnsharpMask",
+            "CIVignette"
+        ]
+        
+        
+        for effect in effects {
+            let action = UIAlertAction(
+                title: effect,
+                style: .default,
+                handler: {
+                    [weak self] action in self?.setFilter(action)
+                }
+            )
+            
+            alert.addAction(action)
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    @IBAction func importPicture(_ sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        
+        picker.dismiss(animated: true)
+        currentImage = image
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        guard let image = currentFilter.outputImage else {
+            return
+        }
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(
+                CIVector(
+                    x: currentImage.size.width / 2,
+                    y: currentImage.size.height / 2
+                ),
+                forKey: kCIInputCenterKey
+            )
+        }
+        
+        guard let cgImage = context.createCGImage(image, from: image.extent) else {
+            return
+        }
+        
+        imageView.image = UIImage(cgImage: cgImage)
     }
 }
 
