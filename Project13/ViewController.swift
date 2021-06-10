089b40b9e1ca7799f29ca086e6a0db1cffed2ea5
Azat <kaumov.a.r@gmail.com>
2021-06-10 08:25:56 +0300

    Add views

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
 }
 
